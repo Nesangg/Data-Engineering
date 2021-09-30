8ea10e7c7dfc58795cccb14cba54a5bcfceaf34c
Nesangg <91529915+Nesangg@users.noreply.github.com>
2021-09-30 16:32:26 +0200

    Add files via upload
    updated task 7

--- task 07.py
+++ task 07.py
@@ -1,14 +1,23 @@
-#covert a number to celsius and fahrenheit
+#covert celsius to fahrenheit
 
 
 degree_sign = u"\N{DEGREE SIGN}"
 
 def conv(c):
     fahrenheit=(c * 9/5) + 32
+    #celsius = (c - 32) * 5/9
+    #print("Celsius entered:       ",c,degree_sign, "C")
+    #fahrenheit=(fahrenheit,degree_sign,"F")
+    return fahrenheit
+    #print("Convert to celsius:   ",celsius,degree_sign, "C")
+print(conv(50));
+
+#covert fahrenheit to celsius
+def conv2(c):
+    #fahrenheit=(c * 9/5) + 32
     celsius = (c - 32) * 5/9
-    print("Number entered:       ",c)
-    print("Convert to fahrenheit:",fahrenheit,degree_sign, "F")
-    print("Convert to celsius:   ",celsius,degree_sign, "C")
-
-
-conv(63)
+    #print("Fahrenheit entered:    ",c,degree_sign, "F")
+    #print("Convert to fahrenheit:",fahrenheit,degree_sign, "F")
+    #celsius=(celsius + degree_sign +"C")
+    return celsius
+print(conv2(120));
